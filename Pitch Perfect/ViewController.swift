ef14c2ba98766cf2fbf93105cedee09404415ee8
hadv <dvietha@gmail.com>
2015-03-19 23:41:58 +0700

    commit the 1st version of Pitch Perfect

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingInProgress: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: UIButton) {
+        //TODO: Record the user' voice
+        recordingInProgress.hidden = false
+        
+    }
 
+    @IBAction func stopRecord(sender: AnyObject) {
+        recordingInProgress.hidden = true
+    }
 }
 
